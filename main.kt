23d624529b15abc40167a62feed8af795b1ccfd8
Samuel Peña <samuelpena098@gmail.com>
2024-09-25 11:38:10 -0400

    update triggers.yml and main program

--- main.kt
+++ main.kt
@@ -1,5 +1,20 @@
 
 
 fun main() {
-    println("Hello, World!")
+    val name = System.getenv("USERNAME")
+    val favouriteLanguage = System.getenv("LANGUAGE")
+
+
+
+    when {
+        name != null && favouriteLanguage != null -> {
+            println("Hello, $name! Your favourite language is $favouriteLanguage")
+        }
+        name != null -> {
+            println("Hello, $name!")
+        }
+        else -> {
+            println("Hello, World!")
+        }
+    }
 }
